88a6e6b9869ec9b23f69519351a6830b0ff558f2
JohanFernandoLema <johan.13fernando@gmail.com>
2023-04-17 11:41:21 -0400

    adjust responsiveness for small devices

---
+++ app.js
@@ -0,0 +1,6 @@
+const openFilters = document.querySelector("openButton");
+const sideFilters = documetn.querySelector("aside");
+
+openFilters.addEventListener('click', () =>{
+    sideFilters.style.display = "block";
+})
