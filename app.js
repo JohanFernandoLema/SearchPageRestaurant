4b94f3534b07db2b4d74124931c238e25fbcd0ca
JohanFernandoLema <johan.13fernando@gmail.com>
2023-04-17 12:20:21 -0400

    show filters

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const closeBtn = document.querySelector("#open-btn");
-const sideFilters = document.querySelector("aside");
+const sideFilters = document.querySelector("#hideRight");
+const openBtn = document.querySelector("#open-btn");
 
 closeBtn.addEventListener('click', () =>{
     sideFilters.style.display = "none";
+})
+
+openBtn.addEventListener('click', () =>{
+    sideFilters.style.display = "block";
 })
