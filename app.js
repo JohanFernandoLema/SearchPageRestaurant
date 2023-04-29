48849d8f86590ff6ae7bf28be3130cfeaa13cc29
JohanFernandoLema <johan.13fernando@gmail.com>
2023-04-29 18:03:16 -0400

    sort option added; however is only working in the dev tools

--- app.js
+++ app.js
@@ -13,14 +13,20 @@ const asianCuisine = [
         cuisine: "chinese",
         mealName: "sweet and sour pork",
         priceForTwo: 1200,
-        desc: "Sashimi is a Japanese delicacy consisting of fresh raw fish or meat sliced into thin pieces and often eaten with soy sauce."
+        desc: "A classic Chinese sweet and sour pork stir-fry recipe made with juicy pieces of pork tenderloin, bell peppers, onion, and pineapple."
     },
 ]
 
 const menuDisplay = document.querySelector('.menu-display');
+const filterByPrice = document.querySelectorAll('.filterPrice');
 
 window.addEventListener('DOMContentLoaded', function(){
-    let menuItems = asianCuisine.map(function(meal){
+   displayItems(asianCuisine);
+})
+
+//Display items
+function displayItems(arrayOfItems){
+    let menuItems = arrayOfItems.map(function(meal){
         return `
         <div class="cards">
             <img src=${meal.img} alt="sashimi" class="photo">
@@ -35,5 +41,28 @@ window.addEventListener('DOMContentLoaded', function(){
     });
     menuItems = menuItems.join('');
     menuDisplay.innerHTML = menuItems;
-    console.log(menuItems);
-})
+    // console.log(menuItems);
+}
+
+//Sort items by their price
+filterByPrice.forEach(function(priceCheck){
+    priceCheck.addEventListener('click', function(e){
+        const priceSort = e.currentTarget.dataset.id;
+        const priceComparison = asianCuisine.filter(function(priceToSort){
+            // console.log(priceToSort.priceForTwo);
+            return priceToSort;
+        });
+        // console.log(priceComparison);
+        const sortItems = priceComparison.sort(function(a,b){
+            if(priceSort == "low"){
+                return b.priceForTwo - a.priceForTwo;
+            }
+            else{
+                return a.priceForTwo - b.priceForTwo;
+            }
+            
+        })
+        console.log(sortItems);
+    });
+    
+});
