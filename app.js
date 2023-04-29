413707633651cb03f6ece327b19a9f4cecbac303
JohanFernandoLema <johan.13fernando@gmail.com>
2023-04-29 10:10:36 -0400

    modification in all files

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const closeBtn = document.querySelector("#open-btn");
-const sideFilters = document.querySelector("#hideRight");
+const sideFilters = document.querySelector(".openAside");
 const openBtn = document.querySelector("#open-btn");
 
 closeBtn.addEventListener('click', () =>{
     sideFilters.style.display = "none";
 })
 
-openBtn.addEventListener('click', () =>{
-    sideFilters.style.display = "block";
-})
